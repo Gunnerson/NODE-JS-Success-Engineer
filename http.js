1823a041296527be3ec07a9ec08ca2f1ebeff9bc
sdorani <seif@engagio.com>
2018-01-04 16:18:47 -0800

    The HTTP module can create an HTTP server that listens to server ports and gives a response back to the client

--- http.js
+++ http.js
@@ -1,3 +1,6 @@
+//Node.js has a built-in module called HTTP, which allows Node.js to transfer data over the Hyper Text Transfer Protocol (HTTP).
+
+//To include the HTTP module, use the require() method:
 var http = require('http');
 
 //create a server object:
